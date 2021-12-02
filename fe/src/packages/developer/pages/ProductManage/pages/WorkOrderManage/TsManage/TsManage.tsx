f50252c8618ad0094240e18b64cb93c81df7e663
Eick <oe52920@gmail.com>
2021-12-02 21:56:29 +0800

    fix: 删除less

--- fe/src/packages/developer/pages/ProductManage/pages/WorkOrderManage/TsManage/TsManage.tsx
+++ fe/src/packages/developer/pages/ProductManage/pages/WorkOrderManage/TsManage/TsManage.tsx
@@ -1,65 +1,55 @@
-import {
-  Button,
-  Card,
-  notification,
-  PageHeader,
-  Popconfirm,
-  Space,
-} from "antd";
-import "./index.less";
-import { useAsync, useAsyncFn } from "react-use";
-import XFormRender from "@/components/NXFormRender/XFormRender";
-import formJson from "./form.json";
-import { useEffect, useRef } from "react";
-import { fetchTsListApi, postRosterApi } from "@/packages/developer/apis";
-import { getSearchParams } from "@gcer/react-air";
-import UserSelect from "@/components/UserSelect/UserSelect";
-import { useRecoilValue } from "recoil";
-import { currentManageProductState } from "developer/recoil";
+import { Button, Card, notification, PageHeader, Popconfirm, Space } from 'antd'
+import { useAsync, useAsyncFn } from 'react-use'
+import XFormRender from '@/components/NXFormRender/XFormRender'
+import formJson from './form.json'
+import { useEffect, useRef } from 'react'
+import { fetchTsListApi, postRosterApi } from '@/packages/developer/apis'
+import { getSearchParams } from '@gcer/react-air'
+import UserSelect from '@/components/UserSelect/UserSelect'
+import { useRecoilValue } from 'recoil'
+import { currentManageProductState } from 'developer/recoil'
 
 export default function TsManage() {
-  const formRef = useRef<any>();
-  const currentManageProduct = useRecoilValue(currentManageProductState);
+  const formRef = useRef<any>()
+  const currentManageProduct = useRecoilValue(currentManageProductState)
   useEffect(() => {
-    const { key } = getSearchParams(location.search);
+    const { key } = getSearchParams(location.search)
     fetchTsListApi({
-      appCode: key,
+      appCode: key
     }).then(({ data }) => {
       const obj =
         data[0]?.dutyUsers.reduce((duty: any, current: any) => {
-          duty[current.level] = current.userNames.map(
-            ({ userName, realName }: any) => ({
-              value: userName,
-              label: realName,
-            })
-          );
-          return duty;
-        }, {}) ?? {};
-      formRef.current.setFieldsValue(obj);
-    });
-  }, []);
+          duty[current.level] = current.userNames.map(({ userName, realName }: any) => ({
+            value: userName,
+            label: realName
+          }))
+          return duty
+        }, {}) ?? {}
+      formRef.current.setFieldsValue(obj)
+    })
+  }, [])
 
   const [{ loading }, submit] = useAsyncFn(async () => {
-    const values = await formRef.current.validateAll().catch(console.log);
-    const { key } = getSearchParams(location.search);
+    const values = await formRef.current.validateAll().catch(console.log)
+    const { key } = getSearchParams(location.search)
     const dutyUsers = Object.entries(values).map(([key, value]: any) => {
       return {
         userNames: value.map((val: any) => ({
           userName: val.value,
-          realName: val.label,
+          realName: val.label
         })),
-        level: key,
-      };
-    });
+        level: key
+      }
+    })
     const params = {
       appCode: key,
-      dutyUsers,
-    };
-    await postRosterApi(params);
+      dutyUsers
+    }
+    await postRosterApi(params)
     notification.success({
-      message: `保存成功！`,
-    });
-  }, []);
+      message: `保存成功！`
+    })
+  }, [])
   return (
     <>
       <PageHeader
@@ -74,12 +64,8 @@ export default function TsManage() {
         }
       />
       <Card bordered={false}>
-        <XFormRender
-          ref={formRef}
-          options={formJson as any}
-          widgets={{ UserSelect }}
-        />
+        <XFormRender ref={formRef} options={formJson as any} widgets={{ UserSelect }} />
       </Card>
     </>
-  );
+  )
 }
